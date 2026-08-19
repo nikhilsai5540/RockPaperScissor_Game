c4a3680744a15bc9d7ac12f0ad3f006e77f10eb4
nikhilsai5540 <nikhilsai5540@gmail.com>
2026-08-19 21:02:19 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+
+choices = ["Rock", "Paper", "Scissor"]
+
+@app.route("/", methods=["GET", "POST"])
+def game():
+
+    user = None
+    computer = None
+    result = None
+
+    if request.method == "POST":
+
+        user = request.form["choice"]
+        computer = random.choice(choices)
+
+        if user == computer:
+            result = "Draw"
+
+        elif user == "Paper" and computer == "Rock":
+            result = "User Win!"
+
+        elif user == "Scissor" and computer == "Paper":
+            result = "User Win!"
+
+        elif user == "Rock" and computer == "Scissor":
+            result = "User Win!"
+
+        else:
+            result = "Computer Win!"
+
+    return render_template(
+        "index.html",
+        user=user,
+        computer=computer,
+        result=result
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
